7236a57ac01262bda442f8398e22eb5e43778036
numb3r3 <wangfelix87@gmail.com>
2022-01-06 13:00:41 +0800

    feat: add pointnet and pointconv

---
+++ models/modules.py
@@ -0,0 +1,135 @@
+"""
+Utility function for PointConv
+Originally from : https://github.com/yanx27/Pointnet_Pointnet2_pytorch/blob/master/utils.py
+Modify by Wenxuan Wu
+Date: September 2019
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DensityNet(nn.Module):
+    def __init__(self, hidden_unit=[16, 8]):
+        super(DensityNet, self).__init__()
+        self.mlp_convs = nn.ModuleList()
+        self.mlp_bns = nn.ModuleList()
+
+        self.mlp_convs.append(nn.Conv2d(1, hidden_unit[0], 1))
+        self.mlp_bns.append(nn.BatchNorm2d(hidden_unit[0]))
+        for i in range(1, len(hidden_unit)):
+            self.mlp_convs.append(nn.Conv2d(hidden_unit[i - 1], hidden_unit[i], 1))
+            self.mlp_bns.append(nn.BatchNorm2d(hidden_unit[i]))
+        self.mlp_convs.append(nn.Conv2d(hidden_unit[-1], 1, 1))
+        self.mlp_bns.append(nn.BatchNorm2d(1))
+
+    def forward(self, density_scale):
+        for i, conv in enumerate(self.mlp_convs):
+            bn = self.mlp_bns[i]
+            density_scale = bn(conv(density_scale))
+            if i == len(self.mlp_convs):
+                density_scale = F.sigmoid(density_scale)
+            else:
+                density_scale = F.relu(density_scale)
+
+        return density_scale
+
+
+class WeightNet(nn.Module):
+
+    def __init__(self, in_channel, out_channel, hidden_unit=[8, 8]):
+        super(WeightNet, self).__init__()
+
+        self.mlp_convs = nn.ModuleList()
+        self.mlp_bns = nn.ModuleList()
+        if hidden_unit is None or len(hidden_unit) == 0:
+            self.mlp_convs.append(nn.Conv2d(in_channel, out_channel, 1))
+            self.mlp_bns.append(nn.BatchNorm2d(out_channel))
+        else:
+            self.mlp_convs.append(nn.Conv2d(in_channel, hidden_unit[0], 1))
+            self.mlp_bns.append(nn.BatchNorm2d(hidden_unit[0]))
+            for i in range(1, len(hidden_unit)):
+                self.mlp_convs.append(nn.Conv2d(hidden_unit[i - 1], hidden_unit[i], 1))
+                self.mlp_bns.append(nn.BatchNorm2d(hidden_unit[i]))
+            self.mlp_convs.append(nn.Conv2d(hidden_unit[-1], out_channel, 1))
+            self.mlp_bns.append(nn.BatchNorm2d(out_channel))
+
+    def forward(self, localized_xyz):
+        # xyz : BxCxKxN
+
+        weights = localized_xyz
+        for i, conv in enumerate(self.mlp_convs):
+            bn = self.mlp_bns[i]
+            weights = F.relu(bn(conv(weights)))
+
+        return weights
+
+
+class PointConvDensityNet(nn.Module):
+    def __init__(self, npoint, nsample, in_channel, mlp, bandwidth, group_all):
+        super(PointConvDensityNet, self).__init__()
+        self.npoint = npoint
+        self.nsample = nsample
+        self.mlp_convs = nn.ModuleList()
+        self.mlp_bns = nn.ModuleList()
+        last_channel = in_channel
+        for out_channel in mlp:
+            self.mlp_convs.append(nn.Conv2d(last_channel, out_channel, 1))
+            self.mlp_bns.append(nn.BatchNorm2d(out_channel))
+            last_channel = out_channel
+
+        self.weightnet = WeightNet(3, 16)
+        self.linear = nn.Linear(16 * mlp[-1], mlp[-1])
+        self.bn_linear = nn.BatchNorm1d(mlp[-1])
+        self.densitynet = DensityNet()
+        self.group_all = group_all
+        self.bandwidth = bandwidth
+
+    def forward(self, xyz, points):
+        """
+        Input:
+            xyz: input points position data, [B, C, N]
+            points: input points data, [B, D, N]
+        Return:
+            new_xyz: sampled points position data, [B, C, S]
+            new_points_concat: sample points feature data, [B, D', S]
+        """
+        B = xyz.shape[0]
+        N = xyz.shape[2]
+        xyz = xyz.permute(0, 2, 1)
+        if points is not None:
+            points = points.permute(0, 2, 1)
+
+        xyz_density = compute_density(xyz, self.bandwidth)
+        inverse_density = 1.0 / xyz_density
+
+        if self.group_all:
+            new_xyz, new_points, grouped_xyz_norm, grouped_density = sample_and_group_all(xyz, points,
+                                                                                          inverse_density.view(B, N, 1))
+        else:
+            new_xyz, new_points, grouped_xyz_norm, _, grouped_density = sample_and_group(self.npoint, self.nsample, xyz,
+                                                                                         points,
+                                                                                         inverse_density.view(B, N, 1))
+        # new_xyz: sampled points position data, [B, npoint, C]
+        # new_points: sampled points data, [B, npoint, nsample, C+D]
+        new_points = new_points.permute(0, 3, 2, 1)  # [B, C+D, nsample,npoint]
+        for i, conv in enumerate(self.mlp_convs):
+            bn = self.mlp_bns[i]
+            new_points = F.relu(bn(conv(new_points)))
+
+        inverse_max_density = grouped_density.max(dim=2, keepdim=True)[0]
+        density_scale = grouped_density / inverse_max_density
+        density_scale = self.densitynet(density_scale.permute(0, 3, 2, 1))
+        new_points = new_points * density_scale
+
+        grouped_xyz = grouped_xyz_norm.permute(0, 3, 2, 1)
+        weights = self.weightnet(grouped_xyz)
+        new_points = torch.matmul(input=new_points.permute(0, 3, 1, 2), other=weights.permute(0, 3, 2, 1)).view(B,
+                                                                                                                self.npoint,
+                                                                                                                -1)
+        new_points = self.linear(new_points)
+        new_points = self.bn_linear(new_points.permute(0, 2, 1))
+        new_points = F.relu(new_points)
+        new_xyz = new_xyz.permute(0, 2, 1)
+
+        return new_xyz, new_points
---
+++ models/pooling.py
@@ -0,0 +1,14 @@
+import torch
+
+
+class Pooling(torch.nn.Module):
+    def __init__(self, pool_type='max'):
+        super(Pooling, self).__init__()
+
+    self.pool_type = pool_type
+
+    def forward(self, input):
+        if self.pool_type == 'max':
+            return torch.max(input, 2)[0].contiguous()
+        elif self.pool_type == 'avg' or self.pool_type == 'average':
+            return torch.mean(input, 2).contiguous()
